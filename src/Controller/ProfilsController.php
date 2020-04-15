077dfc83fb8c7ccc6e197eb413c0e8b05c51416e
galgros <galgros@yahoo.fr>
2020-04-15 21:06:32 +0200

    add a profil carousel on home page, create a profils page and a ProfilsController

---
+++ src/Controller/ProfilsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+class ProfilsController extends AbstractController
+{
+    /**
+     * Display profils page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Profils/profils.html.twig');
+    }
+}
