7c8c315bcd5c7f065d78864832ba1d7a6fd2656c
Cr7t3K <cvr.contact.pro@gmail.com>
2020-04-22 11:15:35 +0200

    create update methode for profils, it's works

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -48,12 +48,42 @@ class ProfilController extends AbstractController
 
     public function setUser($id)
     {
-        $userManager = new UserManager();
-        $currentUser = $userManager->selectOneById($id);
-        $info = $userManager->getUserInfo();
-        return $this->twig->render(
-            'Profil/edit-user-profil.html.twig',
-            ['current_user' => $currentUser, 'info' => $info]
-        );
+        $errors = null;
+
+        if (!empty($_POST)) {
+            $firstName = trim($_POST['first_name']);
+            $lastName = trim($_POST['last_name']);
+            $email = trim($_POST['email']);
+            $zipCode = trim($_POST['zip_code']);
+            $description = trim($_POST['description']);
+            $userId = $id;
+
+            $profil = [
+                'firstName' => $firstName,
+                'lastName' => $lastName,
+                'email' => $email,
+                'zipCode' => $zipCode,
+                'description' => $description,
+            ];
+            if (empty($firstName)) {
+                $errors['firstName'] = "Ce champ est requis";
+            } elseif (empty($lastName)) {
+                $errors['lastName'] = "Ce champ est requis";
+            } elseif (empty($email)) {
+                $errors['email'] = "Ce champ est requis";
+            } elseif (empty($zipCode)) {
+                $errors['zipcode'] = "Ce champ est requis";
+            } elseif (empty($description)) {
+                $errors['description'] = "Ce champ est requis";
+            }
+
+            if (empty($errors)) {
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $userManager = new UserManager();
+                    $userManager->setUserInfo($profil, $userId);
+                    header("Location: /profil/getuser/$id");
+                }
+            }
+        }
     }
 }
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -20,21 +20,49 @@ class UserManager extends AbstractManager
 
     public function getSkills()
     {
-        return $this->pdo->query("SELECT user_id, name FROM users 
-                                            JOIN user_has_skills ON users.id=user_id 
-                                            JOIN skills ON skill_id=skills.id 
-                                            ORDER BY user_id")->fetchAll();
+        return $this->pdo->query(
+            "SELECT user_id, name FROM users 
+                        JOIN user_has_skills ON users.id=user_id 
+                        JOIN skills ON skill_id=skills.id 
+                        ORDER BY user_id"
+        )->fetchAll();
     }
 
     public function getUserInfo()
     {
-        return $this->pdo->query("SELECT * FROM users u 
-                                            JOIN user_has_skills uhs ON u.id = uhs.user_id
-                                            JOIN skills s ON s.id= uhs.skill_id")->fetchAll();
+        return $this->pdo->query(
+            "SELECT * FROM users u 
+                        JOIN user_has_skills uhs ON u.id = uhs.user_id
+                        JOIN skills s ON s.id= uhs.skill_id"
+        )->fetchAll();
     }
 
-    public function setUserInfo()
-    {
 
+    public function setUserInfo(array $profil, int $id) : void
+    {
+        $statement = $this->pdo->prepare(
+            "UPDATE users u
+                        SET 
+                            u.first_name = :first_name, 
+                            u.last_name = :last_name, 
+                            u.email = :email, 
+                            u.zip_code = :zip_code,
+                            u.description = :description 
+                        WHERE 
+                              u.id = :id"
+        );
+
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->bindValue('first_name', $profil['firstName'], \PDO::PARAM_STR);
+        $statement->bindValue('last_name', $profil['lastName'], \PDO::PARAM_STR);
+        $statement->bindValue('email', $profil['email'], \PDO::PARAM_STR);
+        $statement->bindValue('zip_code', $profil['zipCode'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $profil['description'], \PDO::PARAM_STR);
+
+        // TODO insert update value for picture image and banner_image after create upload function
+//        $statement->bindValue('profil_picture', $profil['profil_picture'], \PDO::PARAM_STR);
+//        $statement->bindValue('banner_image', $profil['banner_image'], \PDO::PARAM_STR);
+
+        $statement->execute();
     }
 }
