d00eb02a5b1b8e2e433d5f0ee393d8152a7c7e0d
galgros <galgros@yahoo.fr>
2020-04-28 01:31:15 +0200

    add hover on css for buttons

--- public/assets/js/getImageName.js
+++ public/assets/js/getImageName.js
@@ -1,7 +1,7 @@
 const uploadValue = document.getElementById('profil_picture');
 const labelText = document.getElementById('image_label');
 
-labelText.innerText = 'Choisir une image'
+labelText.innerText = 'Upload photo de profil'
 
 uploadValue.addEventListener('change', () => {
     console.log('value :', uploadValue.value)
