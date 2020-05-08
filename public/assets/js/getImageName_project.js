9b5bd1b831117d4564cfea5a7a00db4f62e75f5a
Cr7t3K <cvr.contact.pro@gmail.com>
2020-05-08 14:03:10 +0200

    Styling form for create project

--- public/assets/js/getImageName_project.js
+++ public/assets/js/getImageName_project.js
@@ -1,10 +1,10 @@
-const pathValue = document.getElementById('profil_picture');
+const pathValueproject = document.getElementById('banner_image');
 const labelText = document.getElementById('image_label');
 
 labelText.innerHTML = '<span id="image_label" class="form-upload-span" style="color: grey">' +
     'Upload photo de profil</span>'
 
-pathValue.addEventListener('change', () => {
-    const pathExplodeValues = pathValue.value.split("\\");
+pathValueproject.addEventListener('change', () => {
+    const pathExplodeValues = pathValueproject.value.split("\\");
     labelText.innerText = pathExplodeValues[2];
 });
