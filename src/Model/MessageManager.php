eac710bc79492b7bc337b4f67e4ca4bc3e1f1223
galgros <galgros@yahoo.fr>
2020-05-09 15:05:11 +0200

    finish message part

--- src/Model/MessageManager.php
+++ src/Model/MessageManager.php
@@ -17,10 +17,18 @@ class MessageManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-    public function selectByUser(int $userId)
+    public function selectByUser($userId)
     {
         // prepared request
-        $statement = $this->pdo->prepare("SELECT * FROM $this->table WHERE to_user=:id ORDER BY id DESC");
+        $statement = $this->pdo->prepare("SELECT m.id, m.author, m.to_user, 
+                                                        m.message, m.created_at, 
+                                                        u.first_name, u.last_name, 
+                                                        u.profil_picture, u.email 
+                                                        FROM $this->table m 
+                                                        JOIN users u 
+                                                        ON m.author=u.id 
+                                                        WHERE to_user=:id 
+                                                        ORDER BY m.id DESC");
         $statement->bindValue('id', $userId, \PDO::PARAM_INT);
         $statement->execute();
 
@@ -60,6 +68,24 @@ class MessageManager extends AbstractManager
                                                     NOW())");
         $statement->bindValue(':author', $message['author'], \PDO::PARAM_INT);
         $statement->bindValue(':project', $message['to_project'], \PDO::PARAM_INT);
+        $statement->bindValue(':message', $message['message'], \PDO::PARAM_STR);
+        $statement->execute();
+    }
+
+    public function createToUser(array $message)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
+                                                    " (author, 
+                                                    to_user,  
+                                                    message, 
+                                                    created_at) 
+                                                    VALUES 
+                                                    (:author, 
+                                                    :user, 
+                                                    :message, 
+                                                    NOW())");
+        $statement->bindValue(':author', $message['author']);
+        $statement->bindValue(':user', $message['to_user']);
         $statement->bindValue(':message', $message['message'], \PDO::PARAM_STR);
         $statement->execute();
     }
